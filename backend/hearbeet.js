e668f206e40c35cf1e638f933a81a315e174e939
Rubén Alejandro Escartín Aparicio <raescartin@gmail.com>
2014-04-15 22:17:18 +0200

    Arreglada creación de directorio /data al iniciar y colecciones iniciales de la base de datos

--- backend/hearbeet.js
+++ backend/hearbeet.js
@@ -1,19 +1,48 @@
 #!/usr/local/bin/node
 
+//Run me second
+//launches heartbeat
+
+//REQUIRES
 var mongodb = require('mongodb');
 var crawler = require('./crawler');
 var dbmanager = require('./dbmanager');
 var jobs = require('./jobs');
 var conf = require('./config');
 var twitta = require('./twitta');
+//var time = require('time');
+var mkdirp = require('mkdirp');
+
+//create directory
+mkdirp('/data', function(err) { 
+    // path was created unless there was error
+});
 
 // Inicializamos el manager
 dbmanager.init(conf.bd);
-dbmanager.connect(startHeartBeed);
+dbmanager.connect(startHeartBeed, function(err, db) {
+	  if (err) throw err;
+	  console.log("Connected to Database");
+
+	  //create collections
+	  db.createCollection("hotspots", function(err, collection){
+		   if (err) throw err;
+		   console.log("hotspots");
+		   console.log(collection);
+	});
+	db.hotspots.ensureIndex({"coordinates":"2dsphere"});
+	db.createCollection("fuegoosh", function(err, collection){
+		   if (err) throw err;
+		   console.log("fuegoosh");
+		   console.log(collection);
+	});
+	db.fuegoosh.ensureIndex({"coordinates":"2dsphere"});
+	});
 
 // Launch core
 function startHeartBeed(err, client) {
-
+	crawler.doEverything();
+	//Create database
     if (!err) {
        // Registra el job que inserta datos raw parseados a la collection
        // HOT_SPOTS 
