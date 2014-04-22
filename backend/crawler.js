a8933d1bca59c5f5aaf9e29fbd6a70d8436a2806
JuanGallego <gallegufo@hotmail.com>
2014-04-22 16:24:47 +0200

    New crawler
    Ahora la base de datos "hotspots" no se borra hasta que no se ha acabado
    de ejecutar el jar, para evitar tenerla vacía tanto tiempo.

--- backend/crawler.js
+++ backend/crawler.js
@@ -7,46 +7,49 @@ var twitta = require('./twitta');
 
 function doEverything() {
     //Create child: Crawler
-    dbmanager.erase(cfg.bd.HOT_SPOTS, function(err){
-        if (err) {
-           console.log("error borrado");
-        } else {
-           makeJavaChild(function(data) {
-              var tmp;
-              var counter = 0;
-              for (var i=0; i<data.length; i++) {
-                 tmp = parseJSON(data[i]);
-                 if (tmp.confidence > cfg.threshold.hotspot) {
-                    dbmanager.insert(cfg.bd.HOT_SPOTS, parseJSON(data[i]), function(){});
-                    counter++;
-                 } 
-              }
-              if (err) return console.error(err);
-              console.log("Introducidos " + counter + " docs");
-              dbmanager.find(cfg.bd.HOT_SPOTS, {confidence:{$gt:cfg.threshold.fire}}, function(err, docs) {
-            	  if (err) return console.error(err);
-	              if (!err){
-	            	  console.log("Twitta ini");
-	            	  twitter.init(docs);
-	              }
-              });
-           });
-        }
+    makeJavaChild(function(data) {
+        // Jar has already finished
+        // The old collection is erased
+        dbmanager.erase(cfg.bd.HOT_SPOTS, function(err){
+            if (err) {
+                console.log("error borrado");
+            } else {
+                var tmp; // 1 doc
+                var counter = 0; // Docs inserted
+                
+                // Insert new docs in database
+                for (var i=0; i<data.length; i++) {
+                    tmp = parseJSON(data[i]);
+                    if (tmp.confidence > cfg.threshold.hotspot) {
+                        dbmanager.insert(cfg.bd.HOT_SPOTS, parseJSON(data[i]), function(){});
+                        counter++;
+                    } 
+                }
+                console.log("Introducidos " + counter + " docs");
+                dbmanager.find(cfg.bd.HOT_SPOTS, {confidence:{$gt:cfg.threshold.fire}}, function(err, docs) {
+                    if (err) return console.error(err);
+                    if (!err){
+                        console.log("Twitta ini");
+                        twitter.init(docs);
+                    }
+                });
+            };
+        });
     });
 }
+
 exports.doEverything = doEverything;
 
 function makeJavaChild(callback) {
     console.log("crawler.makeJavaChild()");
     var spawn = require('child_process').spawn;
     try {
-       var java = spawn('java', ['-jar', cfg.path.java_crawler, cfg.path.data_file], {detached: false, stdio: ['ignore', 'ignore','ignore']});
+        var java = spawn('java', ['-jar', cfg.path.java_crawler, cfg.path.data_file], {detached: false, stdio: ['ignore', 'ignore','ignore']});
         java.on('close', function (code) {
-          console.log("Parsing file.");
-          var data = JSON.parse(fs.readFileSync(cfg.path.data_file, "utf8"));
-          console.log("makeJavaChild.makeJavaCrawler() the crawler dies.");
-          callback(data);
-        
+            console.log("Parsing file.");
+            var data = JSON.parse(fs.readFileSync(cfg.path.data_file, "utf8"));
+            console.log("makeJavaChild.makeJavaCrawler() the crawler dies.");
+            callback(data);
         });
     } catch (e) {
         console.log(e);
@@ -57,16 +60,16 @@ function parseJSON(json) {
     var date = json.acq_date.trim().split("-");
     
     var newjson = {
-          coordinates: {type: "Point", coordinates: [json.longitude, json.latitude]},
-          windSpeed: json.windSpeed,
-          windDirection: json.windDirection,
-          date: new Date(date[0],date[1],date[2],json.acq_time.trim().substring(0,2),json.acq_time.trim().substring(2,4)),
-          confidence: json.confidence,
-          temperature: json.temperature,
-          humidity: json.humidity,
-          vegetation: json.vegetation,
-          frp: json.frp,
-          noise: 0.0
-      };
+        coordinates: {type: "Point", coordinates: [json.longitude, json.latitude]},
+        windSpeed: json.windSpeed,
+        windDirection: json.windDirection,
+        date: new Date(date[0],date[1],date[2],json.acq_time.trim().substring(0,2),json.acq_time.trim().substring(2,4)),
+        confidence: json.confidence,
+        temperature: json.temperature,
+        humidity: json.humidity,
+        vegetation: json.vegetation,
+        frp: json.frp,
+        noise: 0.0
+    };
     return newjson;
 }
